420fb991272adb9475ec17680c8df99205d679d7
VolodiaPG <volodia.parolguarino@gmail.com>
2020-12-06 15:35:44 +0100

    [Java][PI] Add control of parallelism value in the case of the paralell stream

--- PI/Java/src/main/java/ParallelStream.java
+++ PI/Java/src/main/java/ParallelStream.java
@@ -1,3 +1,4 @@
+import java.util.Properties;
 import java.util.stream.IntStream;
 
 public class ParallelStream {
@@ -5,6 +6,13 @@ public class ParallelStream {
         Preprocessing preprocessing = new Preprocessing(args);
         int nb_steps = preprocessing.getNbIterations();
         preprocessing.timerStart();
+
+        // Control the parallelism factor:
+        //      - parallelism > 2 — JDK creates the (# of CPUs - 1) threads for the commonPool
+        //      - parallelism = 1 — JDK creates a new thread for every submitted task
+        //      - parallelism = 0 — a submitted task is executed on a caller thread
+        Properties props = System.getProperties();
+        props.setProperty("java.util.concurrent.ForkJoinPool.common.parallelism", String.valueOf(preprocessing.getNbThreads()));
 
         double sum;
         double steps = 1.0 / (double) nb_steps;
