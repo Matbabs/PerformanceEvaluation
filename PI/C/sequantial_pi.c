a473119df7c69395f98debaa6b0e048ffb225c28
matbabs <matbabs78@gmail.com>
2020-12-07 23:45:21 +0100

    seq C

---
+++ PI/C/sequantial_pi.c
@@ -0,0 +1,18 @@
+#include "./processing.h"
+
+//compile with `gcc sequantial_pi.c processing.h -l pthread -lm -o pthread_pi`
+
+int main (int argc, char *argv[]) { 
+    double pi;
+    initProcessing(argc,argv);
+    long nb_pas = nbIterations;
+
+    timerStart();
+    for(long k=0;k<nb_pas;k++)
+        pi += 4.0 * pow(-1, k) / (2*k + 1);
+    timerStop();
+
+    printf("%f\n",pi);
+    printf("%f\n", timerDuration());
+    return 0;
+}
