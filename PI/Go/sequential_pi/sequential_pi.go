8193b97d80a6d50c0337668e9b243d715fe7909a
matbabs <matbabs78@gmail.com>
2020-12-07 13:01:04 +0100

    param GO

--- PI/Go/sequential_pi/sequential_pi.go
+++ PI/Go/sequential_pi/sequential_pi.go
@@ -3,24 +3,26 @@ package main
 import (
 	"fmt"
 	"math"
+
+	"../processing"
 )
 
-func sequential_pi(n float64) float64 {
-
+func sequentialPi(n float64) float64 {
 	pi := 0.0
-
 	for k := 0.0; k <= float64(n); k++ {
 		pi += 4 * math.Pow(-1, k) / (2*k + 1)
 	}
-
 	return pi
 }
 
 func main() {
 
-	var steps = 1000000
+	processing.InitProcessing()
+	var steps = processing.NbIterations
+	processing.TimerStart()
+	pi := sequentialPi(float64(steps))
+	processing.TimerStop()
 
-	pi := sequential_pi(float64(steps))
 	fmt.Println(pi)
-
+	fmt.Println(processing.TimerDuration())
 }
