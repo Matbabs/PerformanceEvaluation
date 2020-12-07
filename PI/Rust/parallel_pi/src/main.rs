478ad56b5f0eebcf7f880a05c6adadaad0ddb631
matbabs <matbabs78@gmail.com>
2020-12-07 13:36:59 +0100

    Rust Processing

--- PI/Rust/parallel_pi/src/main.rs
+++ PI/Rust/parallel_pi/src/main.rs
@@ -1,5 +1,7 @@
 use std::thread;
 use std::sync::{Arc, Mutex};
+use std::env;
+use std::time::{Instant};
 
 #[derive(Copy, Clone)]
 struct Step{
@@ -9,33 +11,49 @@ struct Step{
 }
 
 fn main() {
-    let nb_steps = 1000000;
-    let nb_threads = 40;
-    let mut steps = Vec::with_capacity(nb_threads);
-    let bloc = nb_steps/nb_threads;
-    let safe_pi = Arc::new(Mutex::new(0.0));
 
-    let handles = (0..nb_threads)
-    .into_iter()
-    .map(|i| {
-        steps.push(Step{
-            start:(bloc*i) as i64,
-            inc:bloc as i64,
-            res:0.0
-        });
-        let pi = Arc::clone(&safe_pi);
-        let mut stps = steps.clone();
-        thread::spawn(move || {
-            for k in stps[i].start..stps[i].start+stps[i].inc{
-                stps[i].res += (4 * i64::pow(-1,k as u32)) as f64 / ((2*k + 1)) as f64;
-            }
-            let mut pi = pi.lock().unwrap();
-            *pi += stps[i].res;
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() == 3 {
+
+        let nb_steps = args[1].parse::<i32>().unwrap();
+        let nb_threads = args[2].parse::<i32>().unwrap();
+        let mut steps = Vec::with_capacity(nb_threads as usize);
+        let bloc = nb_steps/i32::from(nb_threads) ;
+        let safe_pi = Arc::new(Mutex::new(0.0));
+
+        let start_time = Instant::now();
+    
+        let handles = (0..nb_threads)
+        .into_iter()
+        .map(|i| {
+            steps.push(Step{
+                start:(bloc*i) as i64,
+                inc:bloc as i64,
+                res:0.0
+            });
+            let pi = Arc::clone(&safe_pi);
+            let mut stps = steps.clone();
+            let ii = i as usize;
+            thread::spawn(move || {
+                for k in stps[ii].start..stps[ii].start+stps[ii].inc{
+                    stps[ii].res += (4 * i64::pow(-1,k as u32)) as f64 / ((2*k + 1)) as f64;
+                }
+                let mut pi = pi.lock().unwrap();
+                *pi += stps[ii].res;
+            })
         })
-    })
-    .collect::<Vec<thread::JoinHandle<_>>>();  
-    for thread in handles {
-        thread.join().unwrap();
+        .collect::<Vec<thread::JoinHandle<_>>>();  
+        for thread in handles {
+            thread.join().unwrap();
+        }
+
+        let end_time = Instant::now();
+
+        println!("{:?}",safe_pi);
+        println!("{:?}", end_time.duration_since(start_time));
+
+    } else {
+        println!("Usage: <number of iterations> <number of threads>");
     }
-    println!("{:?}",safe_pi);
 }
--- PI/Rust/sequential_pi/src/main.rs
+++ PI/Rust/sequential_pi/src/main.rs
@@ -1,18 +1,33 @@
+use std::env;
+use std::time::{Instant};
 
 fn main() {
 
-    let nb_steps = 1000000;
-    let mut sum: f64 = 0.0;
-    let steps: f64 = 1.0 / nb_steps as f64;
-    let mut x: f64;
-    let p: f64;
+    let args: Vec<String> = env::args().collect();
 
-    for i in 0..nb_steps {
-        x = (i as f64 -0.5)*steps;
-        sum += 4.0 / (1.0 + x * x);
+    if args.len() == 3 {
+
+        let nb_steps = args[1].parse::<i32>().unwrap();
+        let mut sum: f64 = 0.0;
+        let steps: f64 = 1.0 / nb_steps as f64;
+        let mut x: f64;
+        let p: f64;
+
+        let start_time = Instant::now();
+    
+        for i in 0..nb_steps {
+            x = (i as f64 -0.5)*steps;
+            sum += 4.0 / (1.0 + x * x);
+        }
+    
+        p = steps * sum;
+
+        let end_time = Instant::now();
+    
+        println!("{:.64}", p);
+        println!("{:?}", end_time.duration_since(start_time));
+
+    } else {
+        println!("Usage: <number of iterations> <number of threads>");
     }
-
-    p = steps * sum;
-
-    println!("{:.64}", p);
 }
